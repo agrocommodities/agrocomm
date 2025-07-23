0536535ab008805ac36f0ed1b145c9619ec7078a
Lucas Saliés Brum <sistematico@gmail.com>
2025-07-23 15:54:18 -0400

    Updated deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,12 +18,12 @@ mkdir -p $BACKUP_DIR
 
 # Backup da versão atual funcionando
 echo "Fazendo backup da versão atual..."
-sudo /usr/bin/systemctl is-active $SERVICE > /dev/null && {
+# sudo /usr/bin/systemctl is-active $SERVICE > /dev/null && {
     cd $CURRENT_DIR
     mkdir -p $BACKUP_DIR/$DEPLOY_TIMESTAMP
-    cp -a .next node_modules package.json bun.lockb .env.production $BACKUP_DIR/$DEPLOY_TIMESTAMP/ 2>  /dev/null
+    cp -a .next node_modules package.json bun.lockb .env.production $BACKUP_DIR/$DEPLOY_TIMESTAMP/ 2> /dev/null
     echo $DEPLOY_TIMESTAMP > $BACKUP_DIR/last_working_version
-}
+# }
 
 perform_rollback() {
     echo "Falha no deploy. Iniciando rollback..."
