6bb2674153b5d2286eb78b6e8c2b4d9aed23745a
Lucas Saliés Brum <sistematico@gmail.com>
2025-08-13 10:39:06 -0400

    Update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,5 +3,5 @@
 DELETE=""
 
 if [ "$(lsb_release -is)" == "Arch" ] || [ "$(lsb_release -is)" == "VoidLinux" ]; then
-  rsync -avzz $DELETE $HOME/cdn/agrocomm/ nginx@tyche:/var/www/cdn.agrocomm.com.br/
+  rsync -avzz $DELETE $HOME/cdn/agrocomm/ nginx@psique:/var/www/cdn.agrocomm.com.br/
 fi
