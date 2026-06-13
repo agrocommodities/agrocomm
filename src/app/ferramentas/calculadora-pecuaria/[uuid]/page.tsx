0104a74bf9831574b9232b69a774ae69617c1eb1
Lucas Brum <97509+sistematico@users.noreply.github.com>
2026-06-12 23:53:16 -0400

    feat: restore shared calculator with sex mix and city quotes

--- src/app/ferramentas/calculadora-pecuaria/[uuid]/page.tsx
+++ src/app/ferramentas/calculadora-pecuaria/[uuid]/page.tsx
@@ -4,7 +4,7 @@ import Breadcrumb from "@/components/Breadcrumb";
 import { getQuotesByCategory } from "@/actions/quotes";
 import { getSharedCalculation } from "@/actions/shared-calculations";
 import NumericInputEnhancer from "../NumericInputEnhancer";
-import SistemasProdutivosCalculatorV2 from "../SistemasProdutivosCalculatorV2";
+import SistemasProdutivosCalculatorV3 from "../SistemasProdutivosCalculatorV3";
 import ShareCalculatorControls from "../ShareCalculatorControls";
 
 type Props = {
@@ -70,7 +70,7 @@ export default async function SharedCalculatorPage({ params }: Props) {
         className="min-w-0 max-w-full overflow-x-hidden [&_div]:min-w-0 [&_input]:min-w-0 [&_input]:max-w-full [&_label]:min-w-0 [&_section]:min-w-0 [&_section]:max-w-full [&_select]:min-w-0 [&_select]:w-full [&_select]:max-w-full"
       >
         <NumericInputEnhancer />
-        <SistemasProdutivosCalculatorV2 quotes={quotes} />
+        <SistemasProdutivosCalculatorV3 quotes={quotes} />
       </div>
     </div>
   );
