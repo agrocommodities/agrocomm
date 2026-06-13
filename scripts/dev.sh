94f814e50405d87f743f2f0eae457a49b727da97
Lucas Saliés Brum <sistematico@gmail.com>
2026-06-13 08:10:28 -0400

    Refactor code structure for improved readability and maintainability

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,6 +10,5 @@ fi
 tmux new-session -d -s "$SESSION" "pnpm dev"
 tmux set-option -t "$SESSION" remain-on-exit on
 tmux new-window -t "$SESSION"
-tmux new-window -t "$SESSION" "ssh root@psique"
-tmux new-window -t "$SESSION" "ssh nginx@psique"
+tmux new-window -t "$SESSION" "ssh root@psique ; cd /var/www/agrocomm"
 tmux attach-session -t "$SESSION"
